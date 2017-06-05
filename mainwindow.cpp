df47a04a5e341efe301ff75d557357886881ba7a
micheldavalos <michel.davalos@academicos.udg.mx>
2017-06-05 17:27:06 -0500

    Agregada interfaz y probando las signals

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->botonEnviar, SIGNAL(clicked(bool)), this, SLOT(enviar()));
+    connect(&arduino, SIGNAL(datos(QByteArray)), this, SLOT(recibirDatos(QByteArray)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::recibirDatos(const QByteArray &datos)
+{
+    qDebug() << datos;
+}
+
+void MainWindow::enviar()
+{
+    qDebug() << "click";
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "arduino.h"
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,11 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    Arduino arduino;
+
+public slots:
+    void recibirDatos(const QByteArray&datos);
+    void enviar();
 };
 
 #endif // MAINWINDOW_H
